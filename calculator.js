8f6f46a04a1a686e7137667b554ea21311431101
humanathome <eabicic@gmail.com>
2022-05-16 17:19:26 +0200

    refactor math operations chaining, remove unnecessary math functions

--- calculator.js
+++ calculator.js
@@ -2,22 +2,17 @@ const equalsBtn = document.getElementById('equals');
 const deleteBtn = document.getElementById('clear-entry');
 const clearAllBtn = document.getElementById('clear-all');
 
-const operatorDisplay = document.getElementById('current-operator-display');
-const equalsSignDisplay = document.getElementById('equals-sign-display');
-const resultDisplay = document.getElementById('result-display');
-
-const operand1Old = document.getElementById('operand1-old');
-const operatorDisplayOld = document.getElementById('operator-display-old');
-const operand2Old = document.getElementById('operand2-old');
+const oldValuesContainer = document.getElementById('old-values-container');
+const operatorDisplay = document.getElementById('operator-display');
 
 let operand1 = document.getElementById('operand1');
 let operand2 = document.getElementById('operand2');
+let result = '';
 
 // register operator on click
 function registerOperator() {
 	if (operatorDisplay.innerText !== '') operate();
 	operatorDisplay.innerText = this.innerText;
-	
 }
 
 document.querySelectorAll('.operation').forEach(btn => {
@@ -25,10 +20,6 @@ document.querySelectorAll('.operation').forEach(btn => {
 });
 
 // add event listeners to all the number buttons
-document.querySelectorAll('.number').forEach( num => {
-	num.addEventListener('click', registerNumber);
-});
-
 function registerNumber(e) {
 	if (operatorDisplay.innerText === '') {
 		operand1.innerText += e.target.innerText;
@@ -36,6 +27,10 @@ function registerNumber(e) {
 		operand2.innerText += e.target.innerText;
 	}
 }
+
+document.querySelectorAll('.number').forEach( num => {
+	num.addEventListener('click', registerNumber);
+});
 
 equalsBtn.addEventListener('click', operate);
 
@@ -48,60 +43,37 @@ function operate(operator, firstNum, secondNum) {
 	
 	switch (operator) {
 		case "+":
-			equalsSignDisplay.innerText = "=";
-			resultDisplay.innerText = add(firstNum, secondNum);
+			result = firstNum + secondNum;
+			oldValuesContainer.textContent = `${firstNum} + ${secondNum} = `;
 			break;
 		case "-":
-			equalsSignDisplay.innerText = "=";
-			resultDisplay.innerText = subtract(firstNum, secondNum);
+			result = firstNum - secondNum;
+			oldValuesContainer.textContent = `${firstNum} - ${secondNum} = `;
 			break;
 		case "*":
-			equalsSignDisplay.innerText = "=";
-			resultDisplay.innerText = multiply(firstNum, secondNum);
+			result = firstNum * secondNum;
+			oldValuesContainer.textContent = `${firstNum} * ${secondNum} = `;
 			break;
 		case "/":
-			equalsSignDisplay.innerText = "=";
-			resultDisplay.innerText = divide(firstNum, secondNum);
+			result = firstNum / secondNum;
+			oldValuesContainer.textContent = `${firstNum} / ${secondNum} = `;
 			break;
 	}
 	prepareForNewOperation();
 }
 
 function prepareForNewOperation() {
-	operand1Old.innerText = operand1.innerText;
-	operatorDisplayOld.innerText = operatorDisplay.innerText;
-	operand2Old.innerText = operand2.innerText;
-	operand1.innerText = resultDisplay.innerText;
-	operand2.innerText = '';
+	operand1.innerText = result;
 	operatorDisplay.innerText = '';
-}
-
-function add(num1, num2) {
-	return num1 + num2;
-}
-
-function subtract(num1, num2) {
-	return num1 - num2;
-}
-
-function multiply(num1, num2) {
-	return num1 * num2;
-}
-
-function divide(num1, num2) {
-	return num1 / num2;
+	operand2.innerText = '';
 }
 
 // reset operands, operator and results
 function resetOperationValues() {
-	operatorDisplay.innerText = '';
-	operatorDisplayOld.innerText = '';
 	operand1.innerText = '';
+	operatorDisplay.innerText = '';
 	operand2.innerText = '';
-	operand1Old.innerText = '';
-	operand2Old.innerText = '';
-	resultDisplay.innerText = '';
-	equalsSignDisplay.innerText = '';
+	oldValuesContainer.innerText = '';
 }
 
 clearAllBtn.addEventListener('click', resetOperationValues);
